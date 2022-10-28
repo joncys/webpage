dd3c7f30ee07bdeac4e45570180f7650eac70392
Simonas Jončys <373939+joncys@users.noreply.github.com>
2022-10-28 21:18:02 +0300

    Initial directory on next.js

---
+++ pages/index.tsx
@@ -0,0 +1,130 @@
+import '@fontsource/dm-mono'
+
+import type {NextPage} from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import React from 'react'
+
+const Home: NextPage = () => {
+  const mountRef = React.useRef(false)
+  React.useEffect(() => {
+    if (!mountRef.current) {
+      mountRef.current = true
+      console.log('Hey there! :)')
+    }
+  }, [mountRef])
+  return (
+    <div
+      className="w-screen h-screen"
+      style={{
+        backgroundColor: '#1E212A',
+      }}
+    >
+      <div
+        dangerouslySetInnerHTML={{
+          __html: '<!-- This code is great, I know -->',
+        }}
+      />
+      <Head>
+        <title>Simonas Jončys &mdash; I do creative stuff</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div className="flex flex-col gap-1 font-mono text-base p-12">
+        <div className="flex flex-row h-full items-center">
+          <Image src="/sj.png" alt="Photo of me" width="56" height="56" />
+          <div className="flex flex-col ml-6">
+            <div className="text-exta">
+              &#47;&#47; hey there, my name is Simonas Jončys
+            </div>
+            <div className="text-exta">
+              &#47;&#47; this webpage might be under &quot;permanent
+              construction&quot;
+            </div>
+          </div>
+        </div>
+        <div>&nbsp;</div>
+        <div>
+          <span className="text-keyword">build</span>
+          <span className="text-exta">(</span>
+          <a
+            href="https://tromzo.com"
+            target="_blank"
+            rel="noreferrer"
+            className="bg-tromzo text-white"
+          >
+            &apos;tromzo&apos;
+          </a>
+          <span className="text-exta">)</span>
+        </div>
+        <div>&nbsp;</div>
+        <div>
+          <span className="text-keyword">create</span>
+          <span className="text-exta">(</span>
+          <a
+            href="https://atokaita.bandcamp.com/album/atokaita2001"
+            target="_blank"
+            rel="noreferrer"
+            className="bg-atokaita text-white"
+          >
+            &apos;ATOKAITA2001&apos;
+          </a>
+          <span className="text-exta">,&nbsp;</span>
+          <a
+            href="https://30smtnakwrd.bandcamp.com/releases"
+            target="_blank"
+            rel="noreferrer"
+            className="bg-30smtnakwrd text-white"
+          >
+            &apos;30SMTNAKWRD&apos;
+          </a>
+          <span className="text-exta">)</span>
+        </div>
+        <div>&nbsp;</div>
+        <div>
+          <span className="text-keyword">practice</span>
+          <span className="text-exta">(</span>
+          <span className="text-string">&apos;Card magic&apos;</span>
+          <span className="text-exta">,&nbsp;</span>
+          <span className="text-string">&apos;Drums&apos;</span>
+          <span className="text-exta">,&nbsp;</span>
+          <span className="text-string">&apos;Generative Art&apos;</span>
+          <span className="text-exta">)</span>
+        </div>
+        <div>&nbsp;</div>
+        <div>
+          <span className="text-keyword">social</span>
+          <span className="text-exta">(</span>
+          <a
+            href="https://www.linkedin.com/in/joncys/"
+            target="_blank"
+            rel="noreferrer"
+            className="bg-linkedin text-white"
+          >
+            &apos;LinkedIn&apos;
+          </a>
+          <span className="text-exta">,&nbsp;</span>
+          <a
+            href="https://github.com/joncys"
+            target="_blank"
+            rel="noreferrer"
+            className="bg-github text-white"
+          >
+            &apos;GitHub&apos;
+          </a>
+          <span className="text-exta">,&nbsp;</span>
+          <a
+            href="https://twitter.com/joncys"
+            target="_blank"
+            rel="noreferrer"
+            className="bg-twitter text-white"
+          >
+            &apos;Twitter&apos;
+          </a>
+          <span className="text-exta">)</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        string: '#b4db81',
+        keyword: '#91dae6',
+        exta: '#62656c',
+        tromzo: '#d5844a',
+        atokaita: '#4a9dbc',
+        '30smtnakwrd': '#676767',
+        linkedin: '#3375b0',
+        github: '#343434',
+        twitter: '#4D9FEB',
+      },
+    },
+  },
+  plugins: [],
+}
